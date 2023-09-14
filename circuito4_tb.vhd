659f3fd293a982c384e40df42dbfd23a13ee278c
Khalil Santana <khalilsantana@hotmail.com>
2023-09-13 21:42:26 -0300

    Fix formating for tb 4 and 5

--- circuito4_tb.vhd
+++ circuito4_tb.vhd
@@ -3,171 +3,167 @@
 -- Atividade  : Avaliação 1 - Projeto de Circuitos Combinacionais usando VHDL
 -- Alunos     : Khalil G. Q. de Santana, Alisson Boeing
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY circuito4_tb IS
-  -- empty
-END ENTITY;
+entity circuito4_tb is
+   -- empty
+end entity;
 
-ARCHITECTURE arch_1 OF circuito4_tb IS
+architecture arch_1 of circuito4_tb is
 
-  -- DUV component
-  COMPONENT circuito4 IS
-  PORT (
-    -- Input requests
-    i_REQ_0 : IN std_logic;
-    i_REQ_1 : IN std_logic;
-    i_REQ_2 : IN std_logic;
-    i_REQ_3 : IN std_logic;
-    -- Output resutlts
-    o_REQ_0 : OUT std_logic;
-    o_REQ_1 : OUT std_logic;
-    o_REQ_2 : OUT std_logic;
-    o_REQ_3 : OUT std_logic
-  );
-  END COMPONENT;
+   -- DUV component
+   component circuito4 is
+      port (
+         -- Input requests
+         i_REQ_0 : in std_logic;
+         i_REQ_1 : in std_logic;
+         i_REQ_2 : in std_logic;
+         i_REQ_3 : in std_logic;
+         -- Output resutlts
+         o_REQ_0 : out std_logic;
+         o_REQ_1 : out std_logic;
+         o_REQ_2 : out std_logic;
+         o_REQ_3 : out std_logic
+      );
+   end component;
 
-  SIGNAL w_i_REQ_0, w_i_REQ_1, w_i_REQ_2, w_i_REQ_3  : std_logic;
-  SIGNAL w_o_REQ_0, w_o_REQ_1, w_o_REQ_2, w_o_REQ_3  : std_logic;
+   signal w_i_REQ_0, w_i_REQ_1, w_i_REQ_2, w_i_REQ_3 : std_logic;
+   signal w_o_REQ_0, w_o_REQ_1, w_o_REQ_2, w_o_REQ_3 : std_logic;
 
-BEGIN
-  -- Connect DUV
-  u_DUT : circuito4 PORT MAP(
-    i_REQ_0 => w_i_REQ_0,
-    i_REQ_1 => w_i_REQ_1,
-    i_REQ_2 => w_i_REQ_2,
-    i_REQ_3 => w_i_REQ_3,
-    o_REQ_0 => w_o_REQ_0,
-	 o_REQ_1 => w_o_REQ_1,
-	 o_REQ_2 => w_o_REQ_2,
-	 o_REQ_3 => w_o_REQ_3
-  );
+begin
+   -- Connect DUV
+   u_DUT : circuito4 port map(
+      i_REQ_0 => w_i_REQ_0,
+      i_REQ_1 => w_i_REQ_1,
+      i_REQ_2 => w_i_REQ_2,
+      i_REQ_3 => w_i_REQ_3,
+      o_REQ_0 => w_o_REQ_0,
+      o_REQ_1 => w_o_REQ_1,
+      o_REQ_2 => w_o_REQ_2,
+      o_REQ_3 => w_o_REQ_3
+   );
 
-  PROCESS
-  BEGIN
-    w_i_REQ_0 <= '0';
-    w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-	 WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 0000" SEVERITY error;
-	 
-    w_i_REQ_0 <= '0';
-    w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '1')) REPORT "Fail @ 0001" SEVERITY error;
-	 
-	 w_i_REQ_0 <= '0';
-    w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '1')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 0010" SEVERITY error;
+   process
+   begin
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 0000" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '1')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 0011" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '1')) report "Fail @ 0001" severity error;
 
-    w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 0100" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '1') and (w_o_REQ_3 = '0')) report "Fail @ 0010" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 0101" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '1') and (w_o_REQ_3 = '0')) report "Fail @ 0011" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 0110" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 0100" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 0111" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 0101" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1000" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 0110" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1001" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '0') and (w_o_REQ_1 = '1') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 0111" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1010" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1000" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1011" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1001" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1100" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1010" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1101" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1011" severity error;
 
-	 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1110" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1100" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0')  and (w_o_REQ_2 = '0')  and (w_o_REQ_3 = '0')) REPORT "Fail @ 1111" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1101" severity error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1110" severity error;
 
-	 
-    -- Clear inputs
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert((w_o_REQ_0 = '1') and (w_o_REQ_1 = '0') and (w_o_REQ_2 = '0') and (w_o_REQ_3 = '0')) report "Fail @ 1111" severity error;
+      -- Clear inputs
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    ASSERT false REPORT "Test done." SEVERITY note;
-    WAIT;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      assert false report "Test done." severity note;
+      wait;
 
-  END PROCESS;
-END ARCHITECTURE;
+   end process;
+end architecture;
--- circuito5_tb.vhd
+++ circuito5_tb.vhd
@@ -3,159 +3,155 @@
 -- Atividade  : Avaliação 1 - Projeto de Circuitos Combinacionais usando VHDL
 -- Alunos     : Khalil G. Q. de Santana, Alisson Boeing
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY circuito5_tb IS
-  -- empty
-END ENTITY;
+entity circuito5_tb is
+   -- empty
+end entity;
 
-ARCHITECTURE arch_1 OF circuito5_tb IS
+architecture arch_1 of circuito5_tb is
 
-  -- DUV component
-  COMPONENT circuito5 IS
-  PORT (
-    -- Input requests
-    i_REQ_0 : IN std_logic;
-    i_REQ_1 : IN std_logic;
-    i_REQ_2 : IN std_logic;
-    i_REQ_3 : IN std_logic;
-    -- Output resutlts
-    o_REQ : OUT std_logic_vector(1 DOWNTO 0)
+   -- DUV component
+   component circuito5 is
+      port (
+         -- Input requests
+         i_REQ_0 : in std_logic;
+         i_REQ_1 : in std_logic;
+         i_REQ_2 : in std_logic;
+         i_REQ_3 : in std_logic;
+         -- Output resutlts
+         o_REQ : out std_logic_vector(1 downto 0)
 
-  );
-  END COMPONENT;
+      );
+   end component;
 
-  SIGNAL w_i_REQ_0, w_i_REQ_1, w_i_REQ_2, w_i_REQ_3  : std_logic;
-  SIGNAL w_o_REQ:  std_logic_vector(1 DOWNTO 0);
+   signal w_i_REQ_0, w_i_REQ_1, w_i_REQ_2, w_i_REQ_3 : std_logic;
+   signal w_o_REQ                                    : std_logic_vector(1 downto 0);
 
-BEGIN
-  -- Connect DUV
-  u_DUT : circuito5 PORT MAP(
-    i_REQ_0 => w_i_REQ_0,
-    i_REQ_1 => w_i_REQ_1,
-    i_REQ_2 => w_i_REQ_2,
-    i_REQ_3 => w_i_REQ_3,
-    o_REQ => w_o_REQ
-  );
+begin
+   -- Connect DUV
+   u_DUT : circuito5 port map(
+      i_REQ_0 => w_i_REQ_0,
+      i_REQ_1 => w_i_REQ_1,
+      i_REQ_2 => w_i_REQ_2,
+      i_REQ_3 => w_i_REQ_3,
+      o_REQ   => w_o_REQ
+   );
 
-  PROCESS
-  BEGIN
-    w_i_REQ_0 <= '0';
-    w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("11")) REPORT "Fail @ 0001" SEVERITY error;
-	 
-	 w_i_REQ_0 <= '0';
-    w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("10")) REPORT "Fail @ 0010" SEVERITY error;
+   process
+   begin
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = ("11")) report "Fail @ 0001" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("10")) REPORT "Fail @ 0011" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert(w_o_REQ = ("10")) report "Fail @ 0010" severity error;
 
-    w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("01")) REPORT "Fail @ 0100" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = ("10")) report "Fail @ 0011" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("01")) REPORT "Fail @ 0101" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert(w_o_REQ = ("01")) report "Fail @ 0100" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("01")) REPORT "Fail @ 0110" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = ("01")) report "Fail @ 0101" severity error;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("01")) REPORT "Fail @ 0111" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert(w_o_REQ = ("01")) report "Fail @ 0110" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("00")) REPORT "Fail @ 1000" SEVERITY error;
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = ("01")) report "Fail @ 0111" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("00")) REPORT "Fail @ 1001" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert(w_o_REQ = ("00")) report "Fail @ 1000" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("00")) REPORT "Fail @ 1010" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = ("00")) report "Fail @ 1001" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("00")) REPORT "Fail @ 1011" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert(w_o_REQ = ("00")) report "Fail @ 1010" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("00")) REPORT "Fail @ 1100" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = ("00")) report "Fail @ 1011" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("00")) REPORT "Fail @ 1101" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert(w_o_REQ = ("00")) report "Fail @ 1100" severity error;
 
-	 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '0';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = ("00")) REPORT "Fail @ 1110" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = ("00")) report "Fail @ 1101" severity error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '0';
+      wait for 1 ns;
+      assert(w_o_REQ = ("00")) report "Fail @ 1110" severity error;
 
-	 w_i_REQ_0 <= '1';
-	 w_i_REQ_1 <= '1';
-    w_i_REQ_2 <= '1';
-    w_i_REQ_3 <= '1';
-    WAIT FOR 1 ns;
-    ASSERT(w_o_REQ = "00") REPORT "Fail @ 1111" SEVERITY error;
+      w_i_REQ_0 <= '1';
+      w_i_REQ_1 <= '1';
+      w_i_REQ_2 <= '1';
+      w_i_REQ_3 <= '1';
+      wait for 1 ns;
+      assert(w_o_REQ = "00") report "Fail @ 1111" severity error;
+      -- Clear inputs
 
-	 
-    -- Clear inputs
+      w_i_REQ_0 <= '0';
+      w_i_REQ_1 <= '0';
+      w_i_REQ_2 <= '0';
+      w_i_REQ_3 <= '0';
+      assert false report "Test done." severity note;
+      wait;
 
-	 w_i_REQ_0 <= '0';
-	 w_i_REQ_1 <= '0';
-    w_i_REQ_2 <= '0';
-    w_i_REQ_3 <= '0';
-    ASSERT false REPORT "Test done." SEVERITY note;
-    WAIT;
-
-  END PROCESS;
-END ARCHITECTURE;
+   end process;
+end architecture;
